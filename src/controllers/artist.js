00884c5dc8a07f69125b581a3e34d3a6ceb87427
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-08-20 15:35:02 +0100

    Creating artists

---
+++ src/controllers/artist.js
@@ -0,0 +1,15 @@
+const getDb = require('../src/services/db');
+
+exports.create = async (req, res) => {
+    const db = await getDb();
+    const { name, genre } = req.body;
+  
+    try {
+      await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+      res.sendStatus(201);
+    } catch (err) {
+      res.sendStatus(500).json(err);
+    }
+  
+    db.close();
+  };
