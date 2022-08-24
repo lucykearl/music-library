ef35f3cc4c88c42ce2ff99a964105cb40dada5d5
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-08-24 17:54:45 +0100

    Reading a single artist

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -5,4 +5,8 @@ const router = express.Router();
 
 router.post('/', artistController.create);
 
+router.get('/', artistController.read);
+
+router.get('/:artistId', artistController.readById);
+
 module.exports = router;
