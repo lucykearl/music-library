94e7f8e16c9fb1002d05c80b7d9c7794c156f1f0
lucykearl <104901670+lucykearl@users.noreply.github.com>
2022-08-24 17:49:20 +0100

    Creating and Reading artists

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,15 +1,31 @@
-const getDb = require('../src/services/db');
+const getDb = require('../services/db');
 
 exports.create = async (req, res) => {
     const db = await getDb();
     const { name, genre } = req.body;
   
     try {
-      await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
-      res.sendStatus(201);
+        await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
+          name,
+          genre,
+        ]);
+        res.sendStatus(201); 
     } catch (err) {
       res.sendStatus(500).json(err);
     }
   
+    db.close();
+  };
+
+  exports.read = async (_, res) => {
+    const db = await getDb();
+  
+    try {
+      const [artists] = await db.query('SELECT * FROM Artist');
+  
+      res.status(200).json(artists);
+    } catch (err) {
+      res.status(500).json(err);
+    }
     db.close();
   };
--- utils/create-database.js
+++ utils/create-database.js
@@ -33,6 +33,13 @@ const setUpDatabase = async () => {
 
     // create the database if it doesn't already exist
     await db.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`);
+    // utils/create-database.js
+    await db.query(`USE ${DB_NAME}`);
+    await db.query(`CREATE TABLE IF NOT EXISTS Artist (
+    id INT PRIMARY KEY auto_increment,
+    name VARCHAR(25),
+    genre VARCHAR(25)
+)`);
     db.close();
 
   } catch (err) {
@@ -54,10 +61,3 @@ const setUpDatabase = async () => {
 // run the async function
 setUpDatabase();
 
-// utils/create-database.js
-await db.query(`USE ${DB_NAME}`);
-await db.query(`CREATE TABLE IF NOT EXISTS Artist (
-  id INT PRIMARY KEY auto_increment,
-  name VARCHAR(25),
-  genre VARCHAR(25)
-)`);
